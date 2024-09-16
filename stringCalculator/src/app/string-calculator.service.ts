ba6a99883bae04cfadbbd5b0828be52c45dca913
PriyaGupta <pgup9374@gmail.com>
2024-09-16 17:17:03 +0530

    Added string calculator with functionality of simple string calculator with input is a string of comma-separated numbers and output is an integer, sum of the numbers

--- stringCalculator/src/app/string-calculator.service.ts
+++ stringCalculator/src/app/string-calculator.service.ts
@@ -6,9 +6,19 @@ import { Injectable } from '@angular/core';
 export class StringCalculatorService {
 
   constructor() { }
-
+// simple string calculator with input is a string of comma-separated numbers and output is an integer, sum of the numbers
   add(numbers: string): number {
-    return 0
+     if (!numbers) {
+      return 0;
+    }
+
+    // Split the string by commas (or new lines) and convert each number from string to integer
+    const numArray = numbers.split(",").map(num => parseInt(num, 10));
+
+    // Sum the numbers and ignore NaN values
+    return numArray.reduce((sum, currentNum) => {
+      return sum + (isNaN(currentNum) ? 0 : currentNum);
+    }, 0);
     
   }
 }
