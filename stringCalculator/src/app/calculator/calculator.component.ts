12b77dd30f781e39396441bf611258914df9f4ed
PriyaGupta <pgup9374@gmail.com>
2024-09-16 17:04:16 +0530

    Added angular app on addString Calculator with test cases on TDD approach

---
+++ stringCalculator/src/app/calculator/calculator.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { StringCalculatorService } from '../string-calculator.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calculator',
+  standalone: true,
+  imports: [CommonModule,FormsModule],
+  templateUrl: './calculator.component.html',
+  styleUrl: './calculator.component.css'
+})
+export class CalculatorComponent {
+  input: string = '';
+  result: number | null = null;
+  error: string | null = null;
+
+  constructor(private calculatorService: StringCalculatorService) {}
+
+  calculate() {
+    try {
+      this.result = this.calculatorService.add(this.input);
+      this.error = null;
+    } catch (e: any) {
+      this.error = e.message;
+      this.result = null;
+    }
+  }
+}
---
+++ stringCalculator/src/app/string-calculator.service.spec.ts
@@ -0,0 +1,35 @@
+import { TestBed } from '@angular/core/testing';
+
+import { StringCalculatorService } from './string-calculator.service';
+
+describe('StringCalculatorService', () => {
+  let service: StringCalculatorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(StringCalculatorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  it('should return 0 for an empty string', () => {
+    expect(service.add('')).toBe(0);
+  });
+
+  it('should return the number itself for a single number', () => {
+    expect(service.add('5')).toBe(5);
+    expect(service.add('10')).toBe(10);
+  });
+
+  it('should return the sum of two numbers separated by a comma', () => {
+    expect(service.add('1,2')).toBe(3);
+    expect(service.add('10,20')).toBe(30);
+  });
+
+  it('should return the sum of multiple numbers separated by commas', () => {
+    expect(service.add('1,2,3')).toBe(6);
+    expect(service.add('5,10,15')).toBe(30);
+  });
+
+});
